1a08fe279dbe43d79625955b3c127138ccd5bab5
youssef rachidi <youssef.rachidi@indatacore.com>
2023-06-12 00:30:02 +0100

    first commit

--- app/src/main/java/com/example/applicationpfe/SignupActivity.java
+++ app/src/main/java/com/example/applicationpfe/SignupActivity.java
@@ -1,19 +1,35 @@
 package com.example.applicationpfe;
 
 
+import static android.content.ContentValues.TAG;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -23,10 +39,20 @@ public class SignupActivity extends AppCompatActivity {
     FirebaseDatabase database;
     DatabaseReference reference;
 
+    FirebaseFirestore db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        initiateView();
+
+
+    }
+
+    private void initiateView() {
+        db = FirebaseFirestore.getInstance();
 
         signupName = findViewById(R.id.signup_name);
         signupEmail = findViewById(R.id.signup_email);
@@ -35,25 +61,24 @@ public class SignupActivity extends AppCompatActivity {
         loginRedirectText = findViewById(R.id.loginRedirectText);
         signupButton = findViewById(R.id.signup_button);
 
-        signupButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        signupButton.setOnClickListener(view -> {
 
-                database = FirebaseDatabase.getInstance();
-                reference = database.getReference("users");
+            register();
 
-                String name = signupName.getText().toString();
-                String email = signupEmail.getText().toString();
-                String username = signupUsername.getText().toString();
-                String password = signupPassword.getText().toString();
-
-                HelperClass helperClass = new HelperClass(name, email, username, password);
-                reference.child(username).setValue(helperClass);
-
-                Toast.makeText(SignupActivity.this, "You have signup successfully!", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
+//            database = FirebaseDatabase.getInstance();
+//            reference = database.getReference("users");
+//
+//            String name = signupName.getText().toString();
+//            String email = signupEmail.getText().toString();
+//            String username = signupUsername.getText().toString();
+//            String password = signupPassword.getText().toString();
+//
+//            HelperClass helperClass = new HelperClass(name, email, username, password);
+//            reference.child(username).setValue(helperClass);
+//
+//            Toast.makeText(SignupActivity.this, "You have signup successfully!", Toast.LENGTH_SHORT).show();
+//            Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+//            startActivity(intent);
         });
 
         loginRedirectText.setOnClickListener(new View.OnClickListener() {
@@ -63,5 +88,41 @@ public class SignupActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+    }
+
+    private void register() {
+
+        // Get a reference to the Firebase Realtime Database
+        DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference();
+
+// Create a child node to store the user data
+        DatabaseReference usersRef = databaseRef.child("users");
+
+// Create a new User instance
+        HelperClass user = new HelperClass();
+        user.setName("John Doe");
+        user.setEmail("john@example.com");
+        user.setUsername("johndoe");
+        user.setPassword("password123");
+
+// Generate a unique ID for the user
+        String userId = usersRef.push().getKey();
+
+// Save the user data to the database with the generated ID
+        usersRef.child(userId).setValue(user)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // Data successfully saved to Firebase
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Error occurred while saving data to Firebase
+                    }
+                });
+
     }
 }
